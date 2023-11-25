d23b13b28d14daa213bcc643e0da9eb215612509
jortegaruiz <jortegaruiz@gmail.com>
2023-11-24 21:32:24 -0500

    hero-commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-myWhite">
         <header>
           <Nav
             elements={items}
