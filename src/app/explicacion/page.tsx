0c30e954342b87d9c928ca322beb11f4a7d6d15c
jortegaruiz <jortegaruiz@gmail.com>
2024-02-05 13:40:22 -0500

    complete cardinfo

--- src/app/explicacion/page.tsx
+++ src/app/explicacion/page.tsx
@@ -3,9 +3,9 @@ import CardInfo from "../components/CardInfo";
 const data = [
   {
     content:
-      "Una vez extraídos de la mina, los metales preciosos pasan por unproceso de refinación. En este procedimiento, se eliminanimpurezas y se logra una pureza de 24k para el oro y 1000 para laplata, obteniendo así la calidad óptima. Posteriormente, ambosmetales se mezclan cuidadosamente para fortalecer su estructura,resultando en una aleación final de 18k para el oro y 925 para laplata.",
+      "Una vez extraídos de la mina, los metales preciosos pasan por unproceso de refinación. En este procedimiento, se eliminanimpurezas y se logra una pureza de 24k para el oro y 1000 para laplata, obteniendo así la calidad óptima. Posteriormente, ambosmetales se mezclan cuidadosamente para fortalecer su estructura,resultando en una aleación final de 18k para el oro y 925 para la plata.",
     img: "/imagenes/mina.jpeg",
-    alt: "",
+    alt: "foto mina",
     tema: "01 | Materiales",
     title: "Obtención de materia prima",
     title2: "Oro 18k | Plata 925",
@@ -14,14 +14,47 @@ const data = [
   },
   {
     content:
-      "1) Preparación: Toma un trozo de papel estrecho y una regla | 2) Envuelve el papel alrededor del dedo: Envuelve el papel alrededor de la base del dedo y marca donde se encuentran el extremo y el inicio | 3) Mide la longitud: Desenrolla el papel y utiliza la regla para medir la distancia entre la marca y el extremo | 4) Consulta una tabla de tallas: Envia la foto de tu papel marcado a tu asesor.",
+      "1) Preparación: Toma un trozo de papel estrecho y una regla | 2) Envuelve el papel alrededor del dedo: Envuelve el papel alrededor de la base del dedo y marca donde se encuentran el extremo y el inicio | 3) Mide la longitud: Desenrolla el papel y utiliza la regla para medir la distancia entre la marca y el extremo | 4) Enviar imagen: Envia la foto de tu papel marcado a tu asesor.",
     img: "/imagenes/medida.jpg",
-    alt: "",
-    tema: "02 | Cómo saber tu medida?",
+    alt: "foto medida",
+    tema: "02 | Cómo saber su medida?",
     title: "Encuentra su talla de una",
     title2: "manera sencilla",
     color: "text-gray-500",
     number: 2,
+  },
+  {
+    content:
+      "Te ofrecemos servicios de grabado lo cual permite personalizar y hacer única tu joya de manera gratuita. Se pueden grabar fechas, iniciales, o nombres según el modelo, de preferencia que no sea una grabación demasiado extensa ",
+    img: "/imagenes/grabado.jpg",
+    alt: "foto grabado de anillos",
+    tema: "03 | Marca tus anillos?",
+    title: "Haz tus anillos",
+    title2: "unicos",
+    color: "text-yellow-600",
+    number: 3,
+  },
+  {
+    content:
+      "GIA es la principal autoridad mundial en diamantes, piedras de colores y perlas. El Instituto Gemológico Americano también conocido por sus siglas en inglés como “GIA” es una entidad de beneficio público y sin fines de lucro. Representa la principal fuente de conocimiento, estándares y educación en gemas y joyería.",
+    img: "/portada-compromiso.jpg",
+    alt: "foto gia",
+    tema: "04 | Qué es GIA?",
+    title: "Quien tasa los",
+    title2: "diamantes",
+    color: "text-gray-500",
+    number: 4,
+  },
+  {
+    content:
+      "La seguridad en los envíos nacionales es de suma importancia para nosotros. En colaboración con nuestro socio logístico, Servientrega, garantizamos un servicio de entrega confiable y eficiente para nuestros productos, incluyendo tus anillos. Servientrega, reconocida por su amplia experiencia y compromiso con la seguridad, implementa rigurosos protocolos para asegurar la integridad de cada paquete durante todo el proceso logístico.",
+    img: "/imagenes/servientrega.png",
+    alt: "foto servientrega",
+    tema: "05 | Recibe tu envio gratis?",
+    title: "Todos tus pedidos seguros",
+    title2: "a tu puerta",
+    color: "text-green-500",
+    number: 5,
   },
 ];
 
