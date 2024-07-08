7bf77959f95f7a840b5c0a861ed29a398ac9a3fd
jortegaruiz <jortegaruiz@gmail.com>
2024-07-08 15:29:43 -0300

    improve GoogleTagManager component

--- src/app/components/GoogleTagManager.tsx
+++ src/app/components/GoogleTagManager.tsx
@@ -6,9 +6,9 @@ export default function GoogleTagManager() {
         src="https://www.googletagmanager.com/gtag/js?id=AW-10826440933"
       ></script>
       <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
+        {` window.dataLayer = window.dataLayer || []; function gtag()
         {window.dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'AW-10826440933');
+        gtag('js', new Date()); gtag('config', 'AW-10826440933');`}
       </script>
     </>
   );
