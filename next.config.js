46a5710e035b0c0c3bc2dd00112aad7068f29bab
jortegaruiz <jortegaruiz@gmail.com>
2024-07-13 12:31:58 -0300

    jortegaruiz9/cas-37-sitemap

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/sitemap.xml",
+      },
+      {
+        source: "/robots.txt",
+        destination: "/robots.txt",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
