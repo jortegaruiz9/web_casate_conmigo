88942c3d3c5e69e1a5be6dd694878f74538545b0
jortegaruiz <jortegaruiz@gmail.com>
2025-01-31 01:22:27 -0300

    correciones

---
+++ src/app/sitemap/route.ts
@@ -0,0 +1,43 @@
+import { MetadataRoute } from 'next'
+
+export async function GET(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://casateconmigo.ec'
+  
+  // Importaciones dinámicas
+  const { rings: compromiseRings } = await import("../shop/compromiso/Template")
+  const { rings: marriageRings } = await import("../shop/matrimonio/Template")
+  const { rings: cintilloRings } = await import("../shop/cintillos/Template")
+  const { rings: setRings } = await import("../shop/set/Template")
+  
+  const routes = [
+    '',
+    '/shop',
+    '/shop/cintillos',
+    '/shop/compromiso',
+    '/shop/matrimonio',
+    '/shop/set',
+    '/explicacion',
+    '/nosotros',
+  ].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: route === '' ? 1 : 0.8,
+  }))
+
+  const allProducts = [
+    ...compromiseRings,
+    ...marriageRings,
+    ...cintilloRings,
+    ...setRings,
+  ]
+
+  const productPages = allProducts.map(product => ({
+    url: `${baseUrl}/shop/${product.model.toLowerCase()}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.6,
+  }))
+
+  return [...routes, ...productPages]
+} 
